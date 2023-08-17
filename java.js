8eca0930498f21eb961447c3c0b3e50087c50eb5
paritosh22 <122518099+paritosh22@users.noreply.github.com>
2023-08-17 14:10:32 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,26 @@
+let result = "";
+
+
+function appendToResult(value) {
+  result += value;
+  document.getElementById("result").value = result;
+}
+
+
+function clearResult() {
+  result = "";
+  document.getElementById("result").value = "";
+}
+
+
+function calculate() {
+  try {
+    result = eval(result);
+    document.getElementById("result").value = result;
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
+
+
+
